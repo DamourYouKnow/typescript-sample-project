cb9f309bb99d5b8a86f098700e634fbc2381c91b
Bailey D'Amour <bailey@damour.xyz>
2024-06-07 13:43:39 -0400

    Update dependencies

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,9 @@
-import * as path from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express, { Express } from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app: Express = express();
 const port = 8080;
